a3aadc027aeee9e97ef32b5b1b32c261329b9fec
Ivan Kasich <rebelkom@gmail.com>
2025-05-31 04:17:42 +0300

    task11

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+let query = null;
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+hideLoader();
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  query = e.target.elements['search-text'].value.trim();
+  clearGallery();
+  showLoader();
+  getImagesByQuery(query)
+    .then(res => {
+      if (res.data.hits.length === 0) {
+        iziToast.warning({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          color: 'red',
+          position: 'topRight',
+        });
+
+        return;
+      }
+
+      createGallery(res.data.hits);
+      hideLoader();
+      form.reset();
+    })
+    .catch(err => {
+      hideLoader();
+      iziToast.error({
+        message: 'Something went wrong!!!',
+        position: 'topRight',
+      });
+    });
+}
